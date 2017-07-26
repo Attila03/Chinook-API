52e7db3282844a83dd57639fca00bc72e8d914e5
Aditya Vekhande <adityavekhande@gmail.com>
2017-07-27 00:00:12 +0530

    set DEBUG to false

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from .views import UserListView, UserDetailView, RegisterView, LogoutView
 urlpatterns = [
     url(r'^register/$', RegisterView.as_view(), name='register'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
-    url(r'^/users/$', UserListView.as_view(), name='user_list'),
+    url(r'^users/$', UserListView.as_view(), name='user_list'),
     url(r'^users/(?P<pk>[\w-]+)', UserDetailView.as_view(), name='user_detail'),
 ]
